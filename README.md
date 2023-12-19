84ebfee7d44a919d46e1653d4402e3ee0e3c76da
Saeid Mohadjer <saeid@fastmail.fm>
2023-12-19 09:18:47 +0100

    adds api

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-Example of a secure serverless API with basic CRUD functionality that can be accessed from third party domains. JWT token verification is implemented via jose library and middlewares.
+Example of a secure serverless API that can be accessed from any domain. Requests to API should have a valid JWT token in the "Authorization" header. JWT verification is implemented via jose library using an Edge middleware on Vercel.
 
---
+++ middleware.js
@@ -0,0 +1,49 @@
+/* middleware for vercel edge */
+import {jwtVerify} from 'jose';
+import { next } from '@vercel/edge';
+
+export const config = {matcher: '/api:Path*'}
+
+export default async function middleware(req) {
+  const authHeader = req.headers.get('authorization');
+
+  console.log(authHeader);
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+    const secret = new TextEncoder().encode(process.env.secret);
+    console.log(token, process.env.secret);
+
+    try {
+      const payload = await jwtVerify(token, secret);
+      console.log(payload);
+      next();
+    } catch(err) {
+      return Response.json(
+		{
+			'error': JSON.stringify(err)
+		},
+		{
+			status: 401,
+			headers: {
+				'Content-Type': 'application/json',
+				'WWW-Authenticate': 'Bearer'
+			}
+		}
+	  );
+    }
+  } else {
+    return Response.json(
+		{
+			'error': 'authorization header not found'
+		},
+		{
+			status: 401,
+			headers: {
+				'Content-Type': 'application/json',
+				'WWW-Authenticate': 'Bearer'
+			}
+		}
+	);
+  }
+}
