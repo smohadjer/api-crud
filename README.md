d2233ea84220d0e6d6527133e6a47b4775e7a03a
Saeid Mohadjer <saeid@fastmail.fm>
2023-12-19 10:50:37 +0100

    minor

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Example of a secure serverless API that can be accessed from any domain. Requests to API should have a valid JWT token in the "Authorization" header. JWT verification is implemented via jose library using an Edge middleware on Vercel.
 
-https://api.myendpoint.de/api/
+GET https://api-saeid-fastmailfm.vercel.app/api/quotes
+Content-Type: application/json
+Authorization: Bearer jwtAccessToken
 
 
 
--- middleware.js
+++ middleware.js
@@ -5,60 +5,50 @@ import { next } from '@vercel/edge';
 export const config = {matcher: '/api:Path*'}
 
 export default async function middleware(req) {
-  const authHeader = req.headers.get('authorization');
+	const authHeader = req.headers.get('authorization');
 
-  console.log('method:', req.method);
-
-  if (req.method === 'OPTIONS') {
-	//next();
-	return Response.json({
-		'status': 'ok'
-	}, {
-		status: 200,
-		headers: {
-			'Access-Control-Allow-Headers': '*'
-		}
+	// for preflight requests
+	if (req.method === 'OPTIONS') {
+		return Response.json({}, {
+			status: 200,
+			headers: {
+				'Access-Control-Allow-Headers': '*'
+			}
+		});
 	}
-	);
-  }
 
-  console.log(authHeader);
+	if (authHeader) {
+		const accessToken = authHeader.split(' ')[1];
+		const secret = new TextEncoder().encode(process.env.secret);
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
-    const secret = new TextEncoder().encode(process.env.secret);
-    console.log(token, process.env.secret);
-
-    try {
-      const payload = await jwtVerify(token, secret);
-      console.log(payload);
-      next();
-    } catch(err) {
-      return Response.json(
-		{
-			'error': JSON.stringify(err)
-		},
-		{
-			status: 401,
-			headers: {
-				'Content-Type': 'application/json',
-				'WWW-Authenticate': 'Bearer'
-			}
+		try {
+			const payload = await jwtVerify(accessToken, secret);
+			console.log(payload);
+			next();
+		} catch(err) {
+			return Response.json(
+				{'error': JSON.stringify(err)},
+				{
+					status: 401,
+					headers: {
+						'Content-Type': 'application/json',
+						'WWW-Authenticate': 'Bearer'
+					}
+				}
+			);
 		}
-	  );
-    }
-  } else {
-    return Response.json(
-		{
-			'error': 'authorization header not found'
-		},
-		{
-			status: 401,
-			headers: {
-				'Content-Type': 'application/json',
-				'WWW-Authenticate': 'Bearer'
+	} else {
+		return Response.json(
+			{
+				'error': 'authorization header was not found'
+			},
+			{
+				status: 401,
+				headers: {
+					'Content-Type': 'application/json',
+					'WWW-Authenticate': 'Bearer'
+				}
 			}
-		}
-	);
-  }
+		);
+	}
 }
