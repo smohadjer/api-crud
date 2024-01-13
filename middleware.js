b48e7a37c4e3d199685ab88792911e9571bb359b
Saeid Mohadjer <saeid@fastmail.fm>
2024-01-13 12:26:46 +0100

    bug fix

--- middleware.js
+++ middleware.js
@@ -4,13 +4,27 @@ import {jwtVerify} from 'jose';
 /* matcher allows us to define which paths middleware should apply to */
 export const config = {matcher: '/api/:path*'}
 
+const errorResponse = (error) => {
+	return Response.json(
+		{ 'error': error },
+		{
+			status: 401,
+			headers: {
+				'Content-Type': 'application/json',
+				'WWW-Authenticate': 'Bearer'
+			}
+		}
+	);
+};
+
 export default async function middleware(req) {
 	// to disable middleware simply add a return statment
 	// return;
 
 	const authHeader = req.headers.get('authorization');
+	console.log(authHeader);
 
-	// dealing with cors preflight requests
+	// if request is a cors preflight request return 200
 	if (req.method === 'OPTIONS') {
 		return Response.json({}, {
 			status: 200,
@@ -25,33 +39,13 @@ export default async function middleware(req) {
 		const secret = new TextEncoder().encode(process.env.secret);
 
 		try {
-			// if accessToken verification fails following line will throw an error
+			// if accessToken is invalid jwtVerify throws error
 			const payload = await jwtVerify(accessToken, secret);
 			console.log(payload);
 		} catch(err) {
-			return Response.json(
-				{'error': JSON.stringify(err)},
-				{
-					status: 401,
-					headers: {
-						'Content-Type': 'application/json',
-						'WWW-Authenticate': 'Bearer'
-					}
-				}
-			);
+			return errorResponse(JSON.stringify(err));
 		}
 	} else {
-		return Response.json(
-			{
-				'error': 'authorization header was not found'
-			},
-			{
-				status: 401,
-				headers: {
-					'Content-Type': 'application/json',
-					'WWW-Authenticate': 'Bearer'
-				}
-			}
-		);
+		return errorResponse('Authorization header not found!');
 	}
 }
